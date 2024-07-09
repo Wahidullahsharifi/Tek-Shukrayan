9474f751211d81841dea193738fa77ad4ec1492a
Wahidullahsharifi <fausharifi@gmail.com>
2024-07-08 19:22:03 -0700

    Ok I just added one more method

--- src/Shukrayn1/King.java
+++ src/Shukrayn1/King.java
@@ -4,11 +4,7 @@ public class King {
 
     public static void main(String[] args) {
 
-        System.out.println("Hellow Wrold");
-
-        System.out.println("Altaf So you like him ");
-
-        System.out.println("today is Monday");
+        System.out.println("ok I done");
 
 
 
